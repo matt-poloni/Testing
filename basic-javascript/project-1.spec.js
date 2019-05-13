a7d143a332f2e42e6b44fa29df5d9e6327d3b3f1
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-05-13 18:31:29 -0400

    Completes multiplyByTen test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,10 @@
 const helpers = require('./project-1');
 
 // start testing!
+describe('project-1.js', () => {
+  describe('multiplyByTen', () => {
+    it('multiplies the given number by 10', () => {
+      expect(helpers.multiplyByTen(10)).toBe(100);
+    })
+  })
+})
